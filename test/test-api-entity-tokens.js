994a5d8e737bcec7dc11afc45b909dd0e3eebbc0
Travis McHattie <travis.mchattie@thinksolid.com>
2013-08-02 15:54:19 -0700

    Fixed up paths to handle require for the 'app' object.

--- test/test-api-entity-tokens.js
+++ test/test-api-entity-tokens.js
@@ -1,19 +1,42 @@
 module.exports = {
-
     setUp: function (callback) {
-
-        var app = require('../lib/config/app');
-        this.tokens = require('../lib/entities/tokens')(app);
+        this.tokens = require('../lib/entities/tokens');
+        this.userId = '12345';
         callback();
     },
     tearDown: function (callback) {
         // clean up
         callback();
     },
-    testAddToken: function (test) {
-        this.tokens.addToken('token', 'value');
-        console.log();
-        //test.equals(this.foo, 'bar');
+    testCreateTokenRegularUser: function (test) {
+
+        var token = this.tokens.create(this.userId);
+
+        test.equals(token.isValid, true);
+        test.equals(token.isSuper, false);
+        test.equals(token.userId, this.userId);
         test.done();
-    }
+    },
+    testCreateTokenSuperUser: function (test) {
+
+        var token = this.tokens.create(this.userId,true);
+
+        test.equals(token.isValid, true);
+        test.equals(token.isSuper, true);
+        test.equals(token.userId, this.userId);
+        test.done();
+    },
+    testRevokeToken: function (test) {
+
+        var token = this.tokens.create(this.userId,true);
+
+        this.tokens.revoke(token.token);
+
+        token = this.tokens.validate(token.token);
+        test.equals(token.isValid, false);
+        test.equals(token.isSuper, false);
+        test.equals(token.userId, null);
+
+        test.done();
+    },
 };

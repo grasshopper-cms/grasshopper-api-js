ee1160f902a3140c65f4ba08ee57b0c3da8c80db
Travis McHattie <travis.mchattie@thinksolid.com>
2013-10-10 15:53:16 -0700

    Added a user content type to the test database.

--- tasks/mongodb.js
+++ tasks/mongodb.js
@@ -11,7 +11,63 @@ module.exports = function (grunt) {
             { _id: ObjectID("5245ce1d56c02c066b000001"), email: "apitestuser@thinksolid.com", login: "apitestuser", password: "TestPassword", enabled: true, role: "reader", name: "Test User" }
         ],
         contentTypes = [
-            { _id: ObjectID("524362aa56c02c0703000001"), label: "This is my test content type", helpText: "", meta: [], description: "", fields: [{id: "testfield", required: true, instancing: 1, type: "textbox", label: "Title" } ]}
+            { _id: ObjectID("524362aa56c02c0703000001"), label: "This is my test content type", helpText: "", meta: [], description: "", fields: [{id: "testfield", required: true, instancing: 1, type: "textbox", label: "Title" } ]},
+            {
+                _id: ObjectID("5254908d56c02c076e000001"),
+                label: "Users",
+                description: "Protected content type that defines users in the system.",
+                helpText: "These fields are the minimum required to create a user in the system. See more about extending users through plugins.",
+                fields: {
+                    login: {
+                        label: "Login",
+                        type: "textbox",
+                        required: true,
+                        instancing: 1
+                    },
+                    name: {
+                        label: "Name",
+                        type: "textbox",
+                        required: true,
+                        instancing: 1
+                    },
+                    email: {
+                        label: "Email",
+                        type: "textbox",
+                        required: true,
+                        instancing: 1
+                    },
+                    role: {
+                        label: "Role",
+                        type: "dropdown",
+                        required: true,
+                        options: {
+                            items: [
+                                { id: "reader", val: "Reader" },
+                                { id: "author", val: "Author" },
+                                { id: "editor", val: "Editor" },
+                                { id: "admin", val: "Admin" },
+                                { id: "none", val: "None" }
+                            ]
+                        },
+                        instancing: 1
+                    },
+                    password: {
+                        label: "Email",
+                        type: "password",
+                        required: true,
+                        instancing: 1
+                    },
+                    enabled: {
+                        label: "Enabled",
+                        type: "checkbox",
+                        required: true,
+                        instancing: 1
+                    }
+                },
+                meta: [],
+                protected: true
+            }
+
         ],
         content = [],
         nodes = [];

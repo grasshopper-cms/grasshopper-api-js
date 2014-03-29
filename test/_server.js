be14474de824bbebd655100c6db0cd2229d9677a
Travis McHattie <travis.mchattie@thinksolid.com>
2014-03-29 13:43:42 -0700

    start server for tests.

---
+++ test/_server.js
@@ -0,0 +1,13 @@
+describe('api.content', function(){
+    'use strict';
+
+    var environment = require('./config/environment');
+
+    before(function(){
+        require('../lib/grasshopper-api')();
+    });
+
+});
+
+
+

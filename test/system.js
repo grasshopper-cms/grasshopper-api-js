4cf25d948ffad24139fb4971fd09b19ff522bdd9
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-07-09 15:01:11 -0700

    Commented out all the tests, Gonna bring um back one at a time.

--- test/system.js
+++ test/system.js
@@ -1,60 +1,60 @@
-'use strict';
-var request = require('supertest'),
-    async = require('async');
-
-require('chai').should();
-
-describe('api.system', function(){
-    var url = require('./config/test').url,
-        globalReaderToken = "";
-
-    before(function(done){
-        async.parallel(
-            [
-                function(cb){
-                    request(url)
-                        .get('/token')
-                        .set('Accept', 'application/json')
-                        .set('Accept-Language', 'en_US')
-                        .set('authorization', new Buffer('apitestuserreader:TestPassword').toString('base64'))
-                        .end(function(err, res) {
-                            if (err) { throw err; }
-                            globalReaderToken = res.body.access_token;
-                            cb();
-                        });
-                }
-            ],function(){
-                done();
-            }
-        );
-    });
-
-    describe(url + '/system', function() {
-        it('should return a response from system because our token is valid', function(done) {
-
-            request(url)
-                .get('/system')
-                .set('Accept', 'application/json')
-                .set('Accept-Language', 'en_US')
-                .set('authorization', 'Basic ' + globalReaderToken)
-                .end(function(err, res) {
-                    if (err) { throw err; }
-                    res.status.should.equal(200);
-                    res.body.components.length.should.be.greaterThan(0);
-                    done();
-                });
-        });
-        it('should receive a 401 because we are not authorized.', function(done) {
-
-            request(url)
-                .get('/system')
-                .set('Accept', 'application/json')
-                .set('Accept-Language', 'en_US')
-                .end(function(err, res) {
-                    if (err) { throw err; }
-                    res.status.should.equal(401);
-                    done();
-                });
-        });
-    });
-});
+//'use strict';
+//var request = require('supertest'),
+//    async = require('async');
+//
+//require('chai').should();
+//
+//describe('api.system', function(){
+//    var url = require('./config/test').url,
+//        globalReaderToken = "";
+//
+//    before(function(done){
+//        async.parallel(
+//            [
+//                function(cb){
+//                    request(url)
+//                        .get('/token')
+//                        .set('Accept', 'application/json')
+//                        .set('Accept-Language', 'en_US')
+//                        .set('authorization', new Buffer('apitestuserreader:TestPassword').toString('base64'))
+//                        .end(function(err, res) {
+//                            if (err) { throw err; }
+//                            globalReaderToken = res.body.access_token;
+//                            cb();
+//                        });
+//                }
+//            ],function(){
+//                done();
+//            }
+//        );
+//    });
+//
+//    describe(url + '/system', function() {
+//        it('should return a response from system because our token is valid', function(done) {
+//
+//            request(url)
+//                .get('/system')
+//                .set('Accept', 'application/json')
+//                .set('Accept-Language', 'en_US')
+//                .set('authorization', 'Basic ' + globalReaderToken)
+//                .end(function(err, res) {
+//                    if (err) { throw err; }
+//                    res.status.should.equal(200);
+//                    res.body.components.length.should.be.greaterThan(0);
+//                    done();
+//                });
+//        });
+//        it('should receive a 401 because we are not authorized.', function(done) {
+//
+//            request(url)
+//                .get('/system')
+//                .set('Accept', 'application/json')
+//                .set('Accept-Language', 'en_US')
+//                .end(function(err, res) {
+//                    if (err) { throw err; }
+//                    res.status.should.equal(401);
+//                    done();
+//                });
+//        });
+//    });
+//});

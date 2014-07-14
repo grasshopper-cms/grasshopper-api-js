7f2412fc49f6bf39d54b8589a3d5fd9f8a4e21ae
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-07-14 11:13:13 -0700

    Updating environment config.

--- test/config/environment.js
+++ test/config/environment.js
@@ -5,5 +5,7 @@ module.exports = (function(){
     '"db": {"type": "mongodb","host": "mongodb://localhost:27017/test","database": "test","username": "","password": "","debug": false},' +
     '"assets": {"default" : "local","tmpdir" : "' + path.join(__dirname, "../tmp") + '","engines": {"local" : {"path" : "' + path.join(__dirname, "../public") +
     '","urlbase" : "http://localhost/"}}},"identities":{"google" : {"appId" : "123AppId123","secret" : "123Secret123","redirectUrl" : "/guide/login",' +
-    '"scopes" : ["https://www.googleapis.com/auth/userinfo.profile","https://www.googleapis.com/auth/userinfo.email"]}}}';
+    '"scopes" : ["https://www.googleapis.com/auth/userinfo.profile","https://www.googleapis.com/auth/userinfo.email"],' +
+    '"tokenEndpoint" : "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=",' +
+    '"oauthCallback" : "http://localhost:3000/oauth2callback"}}}';
 })();

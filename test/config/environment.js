c18d4e7cb8ce79ad824d383c1dc4d06d4018e28e
Travis McHattie <travis.mchattie@thinksolid.com>
2014-07-30 17:29:05 -0700

    Updated the config with the maximum file size.

--- test/config/environment.js
+++ test/config/environment.js
@@ -1,7 +1,8 @@
 module.exports = (function(){
     'use strict';
+    console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
     var path = require('path');
-    process.env.GRASSHOPPER_CONFIG = '{"cache": {"path": "./cache"},"crypto": {"secret_passphrase" : "223fdsaad-ffc8-4acb-9c9d-1fdaf824af8c"},' +
+    process.env.GRASSHOPPER_CONFIG = '{"server":{"maxFilesSize":3145728},"cache": {"path": "./cache"},"crypto": {"secret_passphrase" : "223fdsaad-ffc8-4acb-9c9d-1fdaf824af8c"},' +
     '"db": {"type": "mongodb","host": "mongodb://localhost:27017/test","database": "test","username": "","password": "","debug": false},' +
     '"assets": {"default" : "local","tmpdir" : "' + path.join(__dirname, "../tmp") + '","engines": {"local" : {"path" : "' + path.join(__dirname, "../public") +
     '","urlbase" : "http://localhost/"}}},"identities":{"google" : {"appId" : "123AppId123","secret" : "123Secret123","redirectUrl" : "/guide/login",' +

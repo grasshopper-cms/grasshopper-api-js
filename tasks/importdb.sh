ca113b56ab97e6405a578e44fec78fe40cb4bdfe
Mike Mostachetti <mike.mostachetti@thinksolid.com>
2015-10-26 12:48:50 -0700

    Moved data into directory and changd the shell script.

--- tasks/importdb.sh
+++ tasks/importdb.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-grunt mongodb:test
+mongoimport -v --drop --db test --collection content --file data/content.json --jsonArray
+mongoimport -v --drop --db test --collection contenttypes --file data/contenttypes.json --jsonArray
+mongoimport -v --drop --db test --collection hookevents --file data/hookevents.json --jsonArray
+mongoimport -v --drop --db test --collection nodes --file data/nodes.json --jsonArray
+mongoimport -v --drop --db test --collection users --file data/users.json --jsonArray

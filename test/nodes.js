8525208b6449a16076ad767a3553938c80c7aa4f
Peter Ajtai <peter.ajtai@thinksolid.com>
2016-06-01 10:29:46 -0700

    Removing tests that have to be refactored

--- test/nodes.js
+++ test/nodes.js
@@ -851,26 +851,6 @@ describe('api.nodes', function(){
     });
 
 
-    describe('POST: ' + url + '/node/:id/assets/move', function() {
-        it('should move one asset to another node.', function(done) {
-
-            request(url)
-                .post('/node/' + testNodeId + '/assets/move')
-                .set('Accept', 'application/json')
-                .set('Accept-Language', 'en_US')
-                .set('authorization', 'Basic ' + globalEditorToken)
-                .send({
-                    newnodeid: testNodeWithNoSubNodes,
-                    filename: 'testimage.png'
-                })
-                .end(function(err, res) {
-                    if (err) { throw err; }
-                    res.status.should.equal(200);
-                    res.body.message.should.equal('Success');
-                    done();
-                });
-        });
-
         /** Requires node level permissions
         it('should fail because the user does not have permissions on the new node id.', function(done) {
             done();
@@ -879,32 +859,6 @@ describe('api.nodes', function(){
         it('should succeed when a user that is a reader but had editor rights on a specific node.', function(done) {
             done();
         });*/
-    });
-
-
-    describe('DELETE: ' + url + '/node/:id/assets/:name', function() {
-        it('should delete an asset with a specific name', function(done) {
-
-             request(url)
-                 .del('/node/' + testNodeWithNoSubNodes + '/assets/testimage.png')
-                 .set('Accept', 'application/json')
-                 .set('Accept-Language', 'en_US')
-                 .set('authorization', 'Basic ' + globalEditorToken)
-                 .end(function(err, res) {
-                     if (err) { throw err; }
-                     res.status.should.equal(200);
-                     res.body.message.should.equal('Success');
-                     done();
-                 });
-         });
-
-         it('should fail because the user does not have permissions.', function(done) {
-             done();
-         });
-
-         it('should succeed when a user that is a reader but had editor rights on a specific node.', function(done) {
-             done();
-         });
     });
 
     describe('DELETE: ' + url + '/node/:id/assets', function() {

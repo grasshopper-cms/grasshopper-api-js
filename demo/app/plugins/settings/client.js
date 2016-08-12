b1f5376e3e9f01eb2ce390b143c93ad89b76ff8c
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2016-08-11 17:44:09 -0700

    Minor cleanup. Prepaing modal service.

--- demo/app/plugins/settings/client.js
+++ demo/app/plugins/settings/client.js
@@ -36,18 +36,37 @@ function handleTabClicked(event) {
 }
 
 function handlePluginCheck() {
-    window.gh.appState
-        .transform('configs.menuItems')
-        .with(function(menuItems, item) {
-            menuItems.push(item);
-            return menuItems;
-        })
-        .using({
-            'showWhenUserRoleIncludes' : 'admin',
-            'name' : 'Dawg',
-            'href' : '/admin/content-types',
-            'iconClasses' : 'fa fa-refresh fa-spin'
-        });
+    window.gh.modalService.showInputModal(); // returns the modal instance.
+
+    // modal.show()
+    //     .then(function() {
+    //         console.log('THEN WAS CALLED');
+    //     })
+    //     .catch(function() {
+    //         console.log('CATCH WAS CALLED');
+    //     });
+
+    // modal.canClose() // called when the user wants to close the modal. return true if it is ok.
+    //     .show() // kicks of the chain returns a promise.
+    //     .then(function() { // use clicked confirm
+    //         // do some work.
+    //         // modal.close();
+    //     })
+    //     .catch(); // user clicked cancel. or closed it.
+
+    // setTimeout(modal.hide, 5000);
+    // window.gh.appState
+    //     .transform('configs.menuItems')
+    //     .with(function(menuItems, item) {
+    //         menuItems.push(item);
+    //         return menuItems;
+    //     })
+    //     .using({
+    //         'showWhenUserRoleIncludes' : 'admin',
+    //         'name' : 'Dawg',
+    //         'href' : '/admin/content-types',
+    //         'iconClasses' : 'fa fa-refresh fa-spin'
+    //     });
 }
 
 domReady(init);

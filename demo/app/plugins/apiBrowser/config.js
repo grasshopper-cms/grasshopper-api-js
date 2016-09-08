065f3057fd8d2d169eea1ab8884bc42628dfdc92
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2016-09-08 14:58:30 -0700

    Api Browser is fixed.

--- demo/app/plugins/apiBrowser/config.js
+++ demo/app/plugins/apiBrowser/config.js
@@ -1,12 +1,24 @@
 'use strict';
 
 var path = require('path'),
-    uuid = require('node-uuid');
+    uuid = require('node-uuid'),
+    version = require(path.join(__dirname, 'package.json')).version;
 
 module.exports = {
     title : 'Api',
-    version : require(path.join(__dirname, 'package.json')).version,
+    version : version,
     description : 'Information About Current Apis',
     directory : 'apiBrowser', // a semi unique id. Make sure is unique amongst plugins
-    id : uuid.v1() // Generated at runtime
+    id : uuid.v1(), // Generated at runtime
+    tabs : [
+        {
+            title : 'Api',
+            active : true,
+            href : '/admin/api-browser',
+            iconclasses : 'fa fa-paper-plane',
+            roles : 'admin reader editor',
+            addedby : `Example Plugin : Version ${version}`,
+            sort : 0
+        }
+    ]
 };
